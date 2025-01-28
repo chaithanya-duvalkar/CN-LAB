2ab3cbbbc549841bdd5a584818a0fb81b36a1f14
Chaithanya S <chaithanya9ig@gmail.com>
2025-01-28 11:19:50 +0530

    partA-code changed

--- sort.java
+++ sort.java
@@ -1,71 +1,85 @@
 import java.util.*;
 
-class sort {
-    static class Frame {
-        String content;
+class sort
+{
+    //initialisation
+    static class Frame
+    {
         int fnum;
+        String content;
 
-        Frame(int n, String S) {
-            this.fnum = n;
-            this.content = S;
-        }
+    Frame(int n,String s)
+    {
+        this.fnum=n;
+        this.content=s;
     }
+}
 
-    // Sorting function using bubble sort
-    public static void sorting(int n, Frame[] F) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (F[j].fnum > F[j + 1].fnum) {
-                    // Swap the frame details (fnum and content)
-                    String s1 = F[j].content, s2 = F[j + 1].content;
-                    int a = F[j].fnum, b = F[j + 1].fnum;
-                    F[j].fnum = b;
-                    F[j + 1].fnum = a;
-                    F[j].content = s2;
-                    F[j + 1].content = s1;
+    //to swap the frames
+    public static void bubblesort(Frame[] F,int n)
+    {
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n-i-1;j++)
+            {
+                if(F[j].fnum>F[j+1].fnum)
+                {
+                    String s1=F[j].content,s2=F[j+1].content;
+                    int a=F[j].fnum,b=F[j+1].fnum;
+
+                    F[j].fnum=b;
+                    F[j+1].fnum=a;
+                    F[j].content=s2;
+                    F[j+1].content=s1;
                 }
             }
         }
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    //read the frame contents
 
-        System.out.println("Enter the number of frames: ");
-        int n = sc.nextInt();
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of frames:");
+        int n=sc.nextInt();
+       
+        System.out.println("Enter frame details");
+        Frame[] F=new Frame[n];
+        for(int i=0;i<n;i++)
+        {
+            System.out.println("Enter frame number:");
+            int a=sc.nextInt();
 
-        // Input frame details
-        System.out.println("Enter the frame details:");
-        Frame[] F = new Frame[n];
-        for (int i = 0; i < n; i++) {
-            System.out.print("\nEnter frame number: ");
-            int a = sc.nextInt();
-            System.out.print("Enter frame content: ");
-            String b = sc.next();
-            F[i] = new Frame(a, b);
+            System.out.println("Enter frame content");
+            String b=sc.next();
+            F[i]=new Frame(a,b);
         }
 
-        // Convert array to a list, shuffle, and then convert back to array
-        List<Frame> frameList = new ArrayList<>(Arrays.asList(F));
+        //shuffle frames
+        List<Frame> frameList=new ArrayList<>(Arrays.asList(F));
         Collections.shuffle(frameList);
-        F = frameList.toArray(new Frame[0]);
+        F=frameList.toArray(new Frame[0]);
 
-        // Display shuffled frame content
-        System.out.println("\nBefore Sorting (Shuffled frames):");
-        for (int i = 0; i < n; i++) {
-            System.out.print(F[i].content + " ");
-        }
-
-        // Sort the frames based on frame number
-        sorting(n, F);
-
-        // Display sorted frame content
-        System.out.println("\n\nAfter Sorting the frames:");
-        for (int i = 0; i < n; i++) {
-            System.out.print(F[i].content + " ");
+        //before sort
+        System.out.println("Frames before sorting:");
+        for(int i=0;i<n;i++)
+        {
+            System.out.print(F[i].content+" ");
         }
         System.out.println();
-
+       
+        bubblesort(F,n);
+        //after sort
+        System.out.println("Frames after sorting:");
+        for(int i=0;i<n;i++)
+        {
+            System.out.print(F[i].content+" ");
+        }
+        System.out.println();
         sc.close();
     }
+
+
+
 }
